4429e69726c09e87c2222a3dc7e0923bbde40fd8
Michael Brooks <drgn24b@gmail.com>
2014-06-16 16:38:16 +0100

    Update UnitTest.php
    Accidentally removed the C after &deg; it's now back in.

--- Cmfcmf/OpenWeatherMap/Util/UnitTest.php
+++ Cmfcmf/OpenWeatherMap/Util/UnitTest.php
@@ -98,7 +98,7 @@ class UnitTest extends \PHPUnit_Framework_TestCase
     {
         $this->givenThereIsAUnitWithUnit("celsius");
 
-        $this->assertSame("&deg;", $this->unit->getUnit());
+        $this->assertSame("&deg;C", $this->unit->getUnit());
     }
 
     public function testFahrenheitFixture()
