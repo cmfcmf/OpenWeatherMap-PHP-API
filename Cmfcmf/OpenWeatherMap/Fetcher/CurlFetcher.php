c770e946520242976af35196c6f3e75a1afe938c
Christian Flach <cmfcmf@users.noreply.github.com>
2015-10-13 10:05:28 +0200

    Allow to set additional Curl options.

--- Cmfcmf/OpenWeatherMap/Fetcher/CurlFetcher.php
+++ Cmfcmf/OpenWeatherMap/Fetcher/CurlFetcher.php
@@ -25,15 +25,32 @@ namespace Cmfcmf\OpenWeatherMap\Fetcher;
 class CurlFetcher implements FetcherInterface
 {
     /**
+     * @var array The Curl options to use. 
+     */
+    private $curlOptions;
+
+    /**
+     * Create a new CurlFetcher instance.
+     * 
+     * @param array $curlOptions The Curl options to use. See http://php.net/manual/de/function.curl-setopt.php
+     * for a list of available options.
+     */
+    public function __construct($curlOptions = [])
+    {
+        $this->curlOptions = $curlOptions;
+    }
+    
+    /**
      * {@inheritdoc}
      */
     public function fetch($url)
     {
         $ch = curl_init($url);
-        $timeout = 5;
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+        curl_setopt_array($ch, $this->curlOptions);
+        
         $content = curl_exec($ch);
         curl_close($ch);
 
